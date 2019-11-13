1f8541af79a06ddc54f657539334a1288dcdc0e9
Eli Bishop <eli@launchdarkly.com>
2019-11-13 14:15:01 -0800

    better regex

--- .ldrelease/update-version.sh
+++ .ldrelease/update-version.sh
@@ -5,5 +5,5 @@ set -e
 TARGET_FILE=ldapi.h
 TEMP_FILE=${TARGET_FILE}.tmp
 
-sed "s/^#define LD_SDK_VERSION \"[^\"]*\"/#define LD_SDK_VERSION \"${LD_RELEASE_VERSION}\"/" "${TARGET_FILE}" > "${TEMP_FILE}"
+sed "s/^#define LD_SDK_VERSION .*/#define LD_SDK_VERSION \"${LD_RELEASE_VERSION}\"/" "${TARGET_FILE}" > "${TEMP_FILE}"
 mv "${TEMP_FILE}" "${TARGET_FILE}"
