2963236889fa4c5c981431fa8b34ab4a46a87ec9
hroederld <hroeder@launchdarkly.com>
2020-07-02 16:16:35 -0700

    [ch80760] windows release build (#135)

--- .ldrelease/test.ps1
+++ .ldrelease/test.ps1
@@ -7,4 +7,4 @@ Import-Module ".\.ldrelease\helpers.psm1" -Force
 SetupVSToolsEnv -architecture amd64
 
 cd build-static
-ExecuteOrFail { cmake --build . --target RUN_TESTS }
+ExecuteOrFail { cmake --build . --target RUN_TESTS --config Release }
