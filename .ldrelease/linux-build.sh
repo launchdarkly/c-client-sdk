c4e69cf36a115b21747602da1f9e812a00072dd0
Elliot <35050275+Apache-HB@users.noreply.github.com>
2020-08-17 12:30:14 -0400

    seperate mac and linux build scripts to add mac specific shared library args

--- .ldrelease/linux-build.sh
+++ .ldrelease/linux-build.sh
@@ -2,10 +2,6 @@
 
 set -uxe
 
-# Releaser runs this script for both the Linux build and the Mac build. If we
-# ever need them to be different, we can instead provide linux-build.sh and
-# mac-build.sh
-
 mkdir -p build-static && cd build-static
 mkdir -p release
 cmake -D CMAKE_INSTALL_PREFIX=./release ..
---
+++ .ldrelease/mac-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -uxe
+
+mkdir -p build-static && cd build-static
+mkdir -p release
+cmake -D CMAKE_INSTALL_PREFIX=./release ..
+cmake --build .
+cmake --build . --target install
+cd ..
+
+mkdir -p build-dynamic && cd build-dynamic
+mkdir -p release
+cmake -D BUILD_TESTING=OFF -D BUILD_SHARED_LIBS=ON CMAKE_INSTALL_PREFIX=./release ..
+cmake --build .
+cmake --build . --target install
+cd ..
