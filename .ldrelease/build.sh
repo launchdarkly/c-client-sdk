a7196efb711961e246f91b3b2bfdcfce11243a94
LaunchDarklyCI <LaunchDarklyCI@users.noreply.github.com>
2020-08-19 09:37:22 -0700

    prepare 2.1.1 release (#60)

--- .ldrelease/build.sh
+++ .ldrelease/build.sh
@@ -2,10 +2,6 @@
 
 set -uxe
 
-# Releaser runs this script for both the Linux build and the Mac build. If we
-# ever need them to be different, we can instead provide linux-build.sh and
-# mac-build.sh
-
 mkdir -p build-static && cd build-static
 mkdir -p release
 cmake -D CMAKE_INSTALL_PREFIX=./release ..
@@ -15,7 +11,7 @@ cd ..
 
 mkdir -p build-dynamic && cd build-dynamic
 mkdir -p release
-cmake -D BUILD_TESTING=OFF -D CMAKE_INSTALL_PREFIX=./release ..
+cmake -D BUILD_TESTING=OFF -D BUILD_SHARED_LIBS=ON -D CMAKE_INSTALL_PREFIX=./release ..
 cmake --build .
 cmake --build . --target install
 cd ..
