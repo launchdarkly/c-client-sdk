0d93f7eee334f33567294fe34bd1fd75e1e3f795
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2021-10-22 09:18:15 -0700

    sc-126967 Exclude google test output from install. (#159)

--- .ldrelease/build.ps1
+++ .ldrelease/build.ps1
@@ -14,86 +14,56 @@ Import-Module ".\.ldrelease\helpers.psm1" -Force
 
 SetupVSToolsEnv -architecture amd64
 
-DownloadAndUnzip -url "https://curl.haxx.se/download/curl-7.59.0.zip" -filename "curl.zip"
+If(Test-Path "curl-7.59.0") {
+    Write-Host "Curl already present."
+} Else {
+    DownloadAndUnzip -url "https://curl.haxx.se/download/curl-7.59.0.zip" -filename "curl.zip"
 
-Write-Host
-Write-Host Building curl
-Push-Location
-cd curl-7.59.0/winbuild
-ExecuteOrFail { nmake /f Makefile.vc mode=static BUILD=release }
-Pop-Location
-
-Write-Host
-Write-Host Building SDK statically release
-Push-Location
-New-Item -ItemType Directory -Force -Path ".\build-static-release"
-cd "build-static-release"
-New-Item -ItemType Directory -Force -Path artifacts
-ExecuteOrFail {
-    cmake -G "Visual Studio 16 2019" -A x64 `
-        -D CMAKE_INSTALL_PREFIX="${PSScriptRoot}/../build-static-release/artifacts" `
-        -D CURL_LIBRARY="${PSScriptRoot}/../curl-7.59.0/builds/libcurl-vc-x64-release-static-ipv6-sspi-winssl/lib/libcurl_a.lib" `
-        -D CURL_INCLUDE_DIR="${PSScriptRoot}/../curl-7.59.0/builds/libcurl-vc-x64-release-static-ipv6-sspi-winssl/include" `
-        ..
+    Write-Host
+    Write-Host Building curl
+    Push-Location
+    cd curl-7.59.0/winbuild
+    ExecuteOrFail { nmake /f Makefile.vc mode=static BUILD=release }
+    Pop-Location
 }
-ExecuteOrFail { cmake --build . --config Release }
-ExecuteOrFail { cmake --build . --target install --config Release }
-Pop-Location
 
-Write-Host
-Write-Host Building SDK dynamically release
-Push-Location
-New-Item -ItemType Directory -Force -Path ".\build-dynamic-release"
-cd "build-dynamic-release"
-New-Item -ItemType Directory -Force -Path artifacts
-ExecuteOrFail {
-    cmake -G "Visual Studio 16 2019" -A x64 `
-        -D BUILD_TESTING=OFF `
-        -D BUILD_SHARED_LIBS=ON `
-        -D CMAKE_INSTALL_PREFIX="${PSScriptRoot}/../build-dynamic-release/artifacts" `
-        -D CURL_LIBRARY="${PSScriptRoot}/../curl-7.59.0/builds/libcurl-vc-x64-release-static-ipv6-sspi-winssl/lib/libcurl_a.lib" `
-        -D CURL_INCLUDE_DIR="${PSScriptRoot}/../curl-7.59.0/builds/libcurl-vc-x64-release-static-ipv6-sspi-winssl/include" `
-        ..
-}
-ExecuteOrFail { cmake --build . --config Release }
-ExecuteOrFail { cmake --build . --target install --config Release }
-Pop-Location
+function Build-Sdk {
+    param (
+        $OutputPath,
+        $BuildStatic,
+        $Config,
+        $BuildTests
+    )
 
-Write-Host
-Write-Host Building SDK statically debug
-Push-Location
-New-Item -ItemType Directory -Force -Path ".\build-static-debug"
-cd "build-static-debug"
-New-Item -ItemType Directory -Force -Path artifacts
-ExecuteOrFail {
-    cmake -G "Visual Studio 16 2019" -A x64 `
-        -D CMAKE_INSTALL_PREFIX="${PSScriptRoot}/../build-static-debug/artifacts" `
-        -D CURL_LIBRARY="${PSScriptRoot}/../curl-7.59.0/builds/libcurl-vc-x64-release-static-ipv6-sspi-winssl/lib/libcurl_a.lib" `
-        -D CURL_INCLUDE_DIR="${PSScriptRoot}/../curl-7.59.0/builds/libcurl-vc-x64-release-static-ipv6-sspi-winssl/include" `
-        ..
-}
-ExecuteOrFail { cmake --build . --config Debug }
-ExecuteOrFail { cmake --build . --target install --config Debug }
-Pop-Location
+    $BUILD_TESTING = If($BuildTests) {"ON"} Else {"OFF"}
+    $BUILD_SHARED = If($BuildStatic) {"OFF"} Else {"ON"}
 
-Write-Host
-Write-Host Building SDK dynamically debug
-Push-Location
-New-Item -ItemType Directory -Force -Path ".\build-dynamic-debug"
-cd "build-dynamic-debug"
-New-Item -ItemType Directory -Force -Path artifacts
-ExecuteOrFail {
-    cmake -G "Visual Studio 16 2019" -A x64 `
-        -D BUILD_TESTING=OFF `
-        -D BUILD_SHARED_LIBS=ON `
-        -D CMAKE_INSTALL_PREFIX="${PSScriptRoot}/../build-dynamic-debug/artifacts" `
-        -D CURL_LIBRARY="${PSScriptRoot}/../curl-7.59.0/builds/libcurl-vc-x64-release-static-ipv6-sspi-winssl/lib/libcurl_a.lib" `
-        -D CURL_INCLUDE_DIR="${PSScriptRoot}/../curl-7.59.0/builds/libcurl-vc-x64-release-static-ipv6-sspi-winssl/include" `
-        ..
+    Write-Host
+    Write-Host Building SDK $(If($BuildStatic) {"statically"} Else {"dynamically"}) ${Config}
+    Push-Location
+    New-Item -ItemType Directory -Force -Path $OutputPath
+    cd $OutputPath
+    New-Item -ItemType Directory -Force -Path artifacts
+    Write-Host
+    ExecuteOrFail {
+        cmake -G "Visual Studio 16 2019" -A x64 `
+            -D BUILD_TESTING=${BUILD_TESTING} `
+            -D BUILD_SHARED_LIBS=${BUILD_SHARED} `
+            -D CMAKE_INSTALL_PREFIX="${PSScriptRoot}/../${OutputPath}/artifacts" `
+            -D CURL_LIBRARY="${PSScriptRoot}/../curl-7.59.0/builds/libcurl-vc-x64-release-static-ipv6-sspi-winssl/lib/libcurl_a.lib" `
+            -D CURL_INCLUDE_DIR="${PSScriptRoot}/../curl-7.59.0/builds/libcurl-vc-x64-release-static-ipv6-sspi-winssl/include" `
+            ..
+    }
+    ExecuteOrFail { cmake --build . --config $Config }
+    ExecuteOrFail { cmake --build . --config $Config --target install }
+    Pop-Location
 }
-ExecuteOrFail { cmake --build . --config Debug }
-ExecuteOrFail { cmake --build . --target install --config Debug }
-Pop-Location
+
+Build-Sdk ./build-static-debug $true "Debug" $false
+Build-Sdk ./build-dynamic-debug $false "Debug" $false
+Build-Sdk ./build-static-release $true "Release" $true
+Build-Sdk ./build-dynamic-release $false "Release" $false
+
 
 # Copy the binary archives to the artifacts directory; Releaser will find them
 # there and attach them to the release (this also makes the artifacts available
